2323ff5bc03c36b1dd7c4a94658a953d88ddf0e4
Gordon Anderson <gordon.b.anderson@gmail.com>
2014-02-05 22:04:44 +0700

    MINOR: Add i18n to field names

--- code/ContactPageMessage.php
+++ code/ContactPageMessage.php
@@ -8,5 +8,17 @@ class ContactPageMessage extends DataObject {
 		'RepliedTo' => 'Boolean'
 	);
 
+
+
+ // tidy up the CMS by not showing these fields
+  public function getCMSFields() {
+ 	$fields = parent::getCMSFields();
+    $fields->renameField('Name', _t('ContactPageMessage.NAME', 'Name'));
+    $fields->renameField('Email', _t('ContactPageMessage.EMAIL', 'Email'));
+    $fields->renameField('Comments', _t('ContactPageMessage.COMMENTS', 'Comments'));
+    $fields->renameField('RepliedTo', _t('ContactPageMessage.REPLIED_TO', 'Replied To'));
+	return $fields;		
+  }
+
 }
 ?>
