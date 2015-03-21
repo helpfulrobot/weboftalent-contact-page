4802cf7240879bab7e9b7438d2d14049782bad00
Gordon Anderson <gordon.b.anderson@gmail.com>
2015-03-21 16:36:04 +0700

    FIX: Whitespace

--- code/ContactPageMessage.php
+++ code/ContactPageMessage.php
@@ -11,14 +11,14 @@ class ContactPageMessage extends DataObject {
 
 
  // tidy up the CMS by not showing these fields
-  public function getCMSFields() {
+	public function getCMSFields() {
  	$fields = parent::getCMSFields();
-    $fields->renameField('Name', _t('ContactPageMessage.NAME', 'Name'));
-    $fields->renameField('Email', _t('ContactPageMessage.EMAIL', 'Email'));
-    $fields->renameField('Comments', _t('ContactPageMessage.COMMENTS', 'Comments'));
-    $fields->renameField('RepliedTo', _t('ContactPageMessage.REPLIED_TO', 'Replied To'));
-	return $fields;		
-  }
+		$fields->renameField('Name', _t('ContactPageMessage.NAME', 'Name'));
+		$fields->renameField('Email', _t('ContactPageMessage.EMAIL', 'Email'));
+		$fields->renameField('Comments', _t('ContactPageMessage.COMMENTS', 'Comments'));
+		$fields->renameField('RepliedTo', _t('ContactPageMessage.REPLIED_TO', 'Replied To'));
+	return $fields;
+	}
 
 }
 ?>
